37ee8e6a32d3ce16f4bf4ae86ec372f760bd40f9
blingbling-110 <523497359@qq.com>
2019-07-18 19:34:49 +0800

    E1_1_4

---
+++ E1_1_4/src/E1_1_4.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class E1_1_4 {
+    public static void main(String[] args) {
+        StdOut.println("a：then多余");
+        StdOut.println("b：if语句缺少括号");
+        StdOut.println("c：没问题");
+        StdOut.println("d：第一句赋值语句缺少分号");
+    }
+}

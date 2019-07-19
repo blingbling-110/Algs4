a56cba6e51ea5556ddc4fe898dd22d6622314b88
blingbling-110 <523497359@qq.com>
2019-07-19 08:01:02 +0800

    找到乱码原因

--- E1_1_4/src/E1_1_4.java
+++ E1_1_4/src/E1_1_4.java
@@ -1,10 +1,8 @@
-import edu.princeton.cs.algs4.StdOut;
-
 public class E1_1_4 {
     public static void main(String[] args) {
-        StdOut.println("a：then多余");
-        StdOut.println("b：if语句缺少括号");
-        StdOut.println("c：没问题");
-        StdOut.println("d：第一句赋值语句缺少分号");
+        System.out.println("a：then多余");
+        System.out.println("b：if语句缺少括号");
+        System.out.println("c：没问题");
+        System.out.println("d：第一句赋值语句缺少分号");
     }
 }

2a9b50b341942a3d6e00de812ef9e5424bc86b38
qinzijun <qinzijun@dias.com.cn>
2020-03-23 16:59:52 +0800

    1.2.11

--- src/com/qzj/C1_2/SmartDate.java
+++ src/com/qzj/C1_2/SmartDate.java
@@ -3,13 +3,19 @@ package com.qzj.C1_2;
 import java.util.Date;
 
 public class SmartDate {
-    private Date date;
+    private Date dateObj;
 
-    public SmartDate() {
-        date = new Date();
+    public SmartDate(int year, int month, int date) {
+        if (year < 1 || month < 1 || month > 12 || date < 1 || date > 31)
+            throw new IllegalArgumentException();
+        else if (date == 31 && month != 1 && month != 3 && month != 5 && month != 7 && month != 8 && month != 10 && month != 12)
+            throw new IllegalArgumentException();
+        else if ((year % 4 != 0 && month == 2 && date > 28) || (year % 4 == 0 && month == 2 && date > 29))
+            throw new IllegalArgumentException();
+        this.dateObj = new Date(year, month, date);
     }
 
-    public SmartDate(long date) {
-        this.date = new Date(date);
+    public static void main(String[] args) {
+        SmartDate smartDate = new SmartDate(2020, 4, 31);
     }
 }
